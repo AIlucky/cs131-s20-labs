ca051448dad2a178c7d49708459190040f0a0f8b
Malte Schwarzkopf <malte@cs.brown.edu>
2020-02-19 22:59:49 -0500

    Release Lab 3

---
+++ lab3/buf/buffer.c
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFSIZE 32
+
+// unused...?
+void hack() {
+  printf("☠️ ☠️ ☠️  You've been HACKED! ☠️ ☠️ ☠️\n");
+  exit(0);
+}
+
+void getCLI_and_tell_me_your_favorite_number() {
+  // stack allocate some variables
+  const int fav_number =
+      12;  // your favorite number is a constant so it can't change
+  char buf[BUFSIZE];
+
+  // print favorite number
+  printf("My favorite number is %d and it will always be %d and "
+         "nothing can change that\n", fav_number, fav_number);
+
+  // get a string from stdin
+  gets(buf);
+
+  // print favorite number again bc its a great number and you want the world to know
+  printf("My favorite number is %d and it will always be %d and "
+         "nothing can change that\n", fav_number, fav_number);
+}
+
+int main(int argc, char **argv) {
+  getCLI_and_tell_me_your_favorite_number();
+
+  printf("Returned to main safe and sound\n");
+
+  return 0;
+}
